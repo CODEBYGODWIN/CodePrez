f202802f4bab79dbedaf1853530f6ee127d4f707
Emmanuel YOHORE <emmanuelbasket01@gmail.com>
2026-04-23 09:42:03 +0200

    component markdown to html

--- prezident/src/renderer/src/App.vue
+++ prezident/src/renderer/src/App.vue
@@ -1,35 +1,12 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import MarkdownIt from 'markdown-it';
-
-const md = new MarkdownIt({
-  html: true,        
-  linkify: true,     
-  typographer: true  
-});
-
-const markdownText = ref<string>('');
-
-const renderedHtml = computed<string>(() => {
-  return md.render(markdownText.value);
-});
+import MarkdownToHtml from './components/MarkdownToHtml.vue';
 
 </script>
 
 <template>
-  <div>
-    <h2>Éditeur Markdown</h2>
-    <textarea v-model="markdownText" rows="8" cols="50"></textarea>
-
-    <h2>Résultat HTML</h2>
-    <div v-html="renderedHtml"></div>
-  </div>
-
+  <MarkdownToHtml />
 </template>
 
 <style scoped>
-textarea {
-  width: 100%;
-  font-family: monospace;
-}
+
 </style>
